77c0f43a74b6f48e649801e16c7bc94d3c8ff7d0
webdevyoo <yjk7454@naver.com>
2024-01-02 11:15:10 +0900

    useEffect - state지정 예제코드 구현

--- react-beginners-cra/src/Button.js
+++ react-beginners-cra/src/Button.js
@@ -5,14 +5,24 @@ import {useState,useEffect} from "react"
 function Button({text}) {
 
   const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
   const onClick=()=>setValue(prev=>prev+1)
-  console.log("i run all the time!")
-  const iRunOnlyOnce = () => console.log("i run only once.")
-  // component가 처음 render할 때 실행되고, 다시는 실행되지 않을 function을 넣는다.
-
-  useEffect(iRunOnlyOnce, [])
+  const onChange=(event)=>setKeyword(event.target.value)
+  useEffect(() => {
+      console.log("I run only once.")
+  }, [])
+  useEffect(() => {
+    console.log("I run when 'keyword' changes", keyword)
+  }, [keyword])
+  useEffect(()=>{
+    console.log("I run when 'counter' changes")
+  }, [counter])
+  useEffect(()=>{
+    console.log("I run when 'keyword' & 'counter' changes")
+  }, [keyword, counter])
   return (
   <div>
+    <input onChange={onChange} value={keyword} type="text" placeholder="Search here..."/>
     <h1>{counter}</h1>
     <button onClick={onClick} className={styles.btn}>{text}</button>
   </div>
