2ead67803f0fee1ffd267902808fa0f6d499abe6
webdevyoo <yjk7454@naver.com>
2024-01-02 12:49:01 +0900

    useEffect - `cleanUp` 함수 구현

--- react-beginners-cra/src/App.js
+++ react-beginners-cra/src/App.js
@@ -1,11 +1,33 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(()=>{
+
+    console.log("crated :)")
+
+    /**
+     * useEffect의 cleanUp
+     * 컴포넌트 destory시 실행된다.
+     */
+    return (()=>{
+      console.log("destroyed :(")
+    })
+
+  }, [])
+  return <h1>Hello</h1>
+}
 
 function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing(prev=>!prev)
   return (
     <div>
       <h1 className={styles.title}>Welcome back!</h1>
       <Button text="Continue"/>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing && <Hello/>}
     </div>
   );
 }
