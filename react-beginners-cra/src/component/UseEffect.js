cb7ff728f720f53153811e3b74190932847460e9
webdevyoo <yjk7454@naver.com>
2024-01-04 10:06:29 +0900

    UseEffect.js 컴포넌트 파일 추가 및 App.js에서 코드 이동

---
+++ react-beginners-cra/src/component/UseEffect.js
@@ -0,0 +1,12 @@
+import Button from "../Button"
+import styles from "../UseEffect.module.css"
+
+export default function UseEffect() {
+  return (
+    <div>
+      <h1>UseEffect Example</h1>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text="Continue"/>
+    </div>
+  )
+}
