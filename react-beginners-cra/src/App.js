a1a85119f8795bb9680ba25b85b1bf306bd4271e
webdevyoo <yjk7454@naver.com>
2024-01-05 09:34:41 +0900

    영화 api 조회 및 데이터 set & 데이터 출력

--- react-beginners-cra/src/App.js
+++ react-beginners-cra/src/App.js
@@ -1,11 +1,35 @@
 import ExamCombine from "./component/example/ExamCombine";
-
+import { useEffect, useState } from "react";
 function App() {
 
- 
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  
+  useEffect(()=>{
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+    .then(response=>response.json())
+    .then(json=>{
+      setMovies(json.data.movies)
+      setLoading(false)
+    })
+  }, [])
+  console.log(movies)
   return (
     <div>
-      <ExamCombine/>
+      {/* <ExamCombine/> */}
+      {loading ? 
+      <h1>Loading...</h1> : 
+      <div>
+        {movies.map(movie=>
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image}/>
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>{movie.genres.map((g, i) => <li key={i}>{g}</li>)}</ul>
+        </div>
+        )}
+      </div>
+      }
     </div>
   );
 }
