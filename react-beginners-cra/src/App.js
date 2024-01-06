f02552a6ea3b7c424b82fecef16e331a6739467d
webdevyoo <yjk7454@naver.com>
2024-01-07 00:11:37 +0900

    App.js에 Router등록 - React-Router-Dom {BrowserRouter, Routes, Route}}

--- react-beginners-cra/src/App.js
+++ react-beginners-cra/src/App.js
@@ -1,53 +1,18 @@
 import ExamCombine from "./component/example/ExamCombine";
-import { useEffect, useState } from "react";
-import Movie from "./component/movie/Movie";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
 
-  const [movies, setMovies] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  const getMoviesThen = () => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-    .then(response=>response.json())
-    .then(json=>{
-      setMovies(json.data.movies)
-      setLoading(false)
-    })
-  }
-
-  const getMoviesAsync = async() => {
-    try {
-    // const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-    // const json = await response.json()
-    /* Short Cut */
-      const json = await((await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json())
-      setMovies(json.data.movies)
-      setLoading(false)
-    } catch (error) {
-      console.log(error)      
-    }
-  }
-  useEffect(() => {
-    // getMoviesThen();
-    getMoviesAsync();
-  }, [])
-
   return (
-    <div>
+    <Router>
       {/* <ExamCombine/> */}
-      {loading ? 
-      <h1>Loading...</h1> :
-      movies.map(movie=> <Movie 
-        key={movie.id}
-        mediumCoverImage={movie.medium_cover_image}
-        title={movie.title}
-        summary={movie.summary}
-        genres={movie.genres}
-        />
-      )
-      }
-    </div>
-  );
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/movie" element={<Detail/>}/>
+        </Routes>
+      </Router>
+    )
 }
 
 export default App;
