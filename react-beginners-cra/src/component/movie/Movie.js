4ee4ca6f4c9c33e1b1a848a744fe94b5fc9fd17c
webdevyoo <yjk7454@naver.com>
2024-01-07 00:12:47 +0900

    Movie.js - 타이틀 Link컴포넌트 적용

--- react-beginners-cra/src/component/movie/Movie.js
+++ react-beginners-cra/src/component/movie/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 export default function Movie({mediumCoverImage, title, summary, genres}) {
 
     return (
       <div>
         <img src={mediumCoverImage} alt={title}/>
-        <h2>{title}</h2>
+        <h2>
+          <Link to="movie">
+            {title}
+          </Link>
+          </h2>
         <p>{summary}</p>
         <ul>{genres.map((g, i) => <li key={i}>{g}</li>)}</ul>
       </div>
