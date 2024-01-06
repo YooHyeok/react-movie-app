694fd5361b368754821d3ed7321975df7fe4f442
webdevyoo <yjk7454@naver.com>
2024-01-06 15:51:17 +0900

    Home.js 컴포넌트 추가 - App.js에 구현된 Movies 코드 홈화면 컴포넌트로 컴포넌트화

---
+++ react-beginners-cra/src/routes/Home.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import Movie from "../movie/Movie";
+export default function Home() {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const getMoviesThen = () => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+    .then(response=>response.json())
+    .then(json=>{
+      setMovies(json.data.movies)
+      setLoading(false)
+    })
+  }
+
+  const getMoviesAsync = async() => {
+    try {
+    // const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+    // const json = await response.json()
+    /* Short Cut */
+      const json = await((await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json())
+      setMovies(json.data.movies)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)      
+    }
+  }
+  useEffect(() => {
+    // getMoviesThen();
+    getMoviesAsync();
+  }, [])
+
+  return (
+    <div>
+      {loading ? 
+      <h1>Loading...</h1> :
+      movies.map(movie=> <Movie 
+        key={movie.id}
+        mediumCoverImage={movie.medium_cover_image}
+        title={movie.title}
+        summary={movie.summary}
+        genres={movie.genres}
+        />
+      )
+      }
+    </div>
+  );
+}
