3b3ec56e770d4573fba2f0c4e9c5ae6aaa1c72c6
webdevyoo <yjk7454@naver.com>
2024-01-08 10:35:27 +0900

    Movie.js title영역 Link에 템플릿 리터럴 문법을 통해 파라미터 적용

--- react-beginners-cra/src/component/movie/Movie.js
+++ react-beginners-cra/src/component/movie/Movie.js
@@ -1,12 +1,11 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-export default function Movie({mediumCoverImage, title, summary, genres}) {
-
+export default function Movie({id, mediumCoverImage, title, summary, genres}) {
     return (
       <div>
         <img src={mediumCoverImage} alt={title}/>
         <h2>
-          <Link to="movie">
+          <Link to={`/movie/${id}`}>
             {title}
           </Link>
           </h2>
@@ -17,6 +16,7 @@ export default function Movie({mediumCoverImage, title, summary, genres}) {
 }
 
 Movie.prototype={
+  id:PropTypes.number.isRequired,
   mediumCoverImage: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
