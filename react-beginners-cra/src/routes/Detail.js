4ac15740206fa6ead07078265646be7058859554
webdevyoo <yjk7454@naver.com>
2024-01-08 10:47:33 +0900

    Detail컴포넌트 useParams 적용 - Route path와 매핑

--- react-beginners-cra/src/routes/Detail.js
+++ react-beginners-cra/src/routes/Detail.js
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+
 export default function Detail() {
+  /**
+   * useParams()
+   * Rotue에서 받는 :변수명과 일치해야한다.
+   * {id:value} 형태와 같이 객체로 넘어오기 때문에 구조분해할당 활용 가능
+   * ex) /movie/:id/:namme - const {id, name} = useParams()
+   **/
+
+  const {id} = useParams(); // 구조분해할당 {id : value}로 넘어온다. 
+  const getMovieApi = async ()=>{
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json()
+      console.log(json.data.movie)
+  }
+  useEffect(()=>{
+    getMovieApi();
+  },[])
   return <h1>Detail</h1>
 }
