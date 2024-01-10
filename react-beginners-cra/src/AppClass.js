26bc02cb9edefea2ef8438c1fb3d278c4985a33d
webdevyoo <yjk7454@naver.com>
2024-01-10 12:51:37 +0900

    Class형 컴포넌트로 수정 - render함수, state 변수 활용

--- react-beginners-cra/src/AppClass.js
+++ react-beginners-cra/src/AppClass.js
@@ -1,55 +1,47 @@
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+import {Component} from 'react';
+
+/**
+ * React Component를 상속받아 App클래스를 확장함으로서
+ * React Component의 State와 Render 등 많은 기능들을 사용할 수 있게 된다.
+ * Class컴포넌트는 React Component 의 render메소드를 통해 렌더링한다.
+ */
+export default class AppClass extends Component{
+
+  /**
+   * Class형 컴포넌트에서 사용가능한 state변수
+   * Object타입으로 컴포넌트가 내부에서 관리하는 데이터를 나타낸다.
+   * 
+   * 즉 컴포넌트 단위별로 특정 변수를 선언해서 관리할때 state를 사용한다.
+   * (변경된 값에 의해 라이프사이클이 작동되야 하는 기능이 있다면 
+   * 해당 기능에 사용되는 변수를 React Component를 상에서 관리하는 
+   * 
+   * Object타입의 state 변수를 React Component를 상속받음으로써 사용한다.)
+   * 주로 동적인 데이터를 다룰 때 사용한다.
+   */
+  state = {
+    count:0
   }
-];
 
-function Food({name, picture}) {
-  console.log(name)
-  return (
-    <>
-      <h2>I Like {name}</h2>
-      <img src={picture}/>
-    </>
+  add = () => {
+    console.log("add")
+  }
+  minus = () => {
+    console.log("minus")
+  }
+
+  /**
+   * Class형 컴포넌트에서 특정 컴포넌트나 HTML 등의 JSX를 렌더링 하기 위한 메소드
+   */
+  render() {
+    return (
+      <div>
+        <h1>
+          The number is: {this.state.count }
+          {/* 클래스 이므로 this키워드로 접근 */}
+        </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
     )
-
+  }
 }
-
-
-function App() {
-  
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
-      <Food />
-      <Food name="ramen"/>
-      <Food name="samgiopsal"/>
-      <Food name="cuhkumi"/>
-    </div>
-    )
-}
-
-export default App;
