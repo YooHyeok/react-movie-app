fe41c914172f0df20c19271723ac75cd696e5c1e
webdevyoo <yjk7454@naver.com>
2024-01-11 15:01:28 +0900

    변경된 디렉토리로 import문 수정

--- react-beginners-cra/src/AppFunc.js
+++ react-beginners-cra/src/AppFunc.js
@@ -1,4 +1,4 @@
-import ExamCombine from "./component/example/ExamCombine";
+import ExamCombine from "./component/funcExam/ExamCombine";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
