b33fee4d227652c2528fa9de15a98e1fb3962d83
webdevyoo <yjk7454@naver.com>
2024-01-12 12:25:05 +0900

    Movie컴포넌트 추가 및 props 렌더링 테스트

--- react-beginners-cra/src/class/component/movie/Movie.js
+++ react-beginners-cra/src/class/component/movie/Movie.js
@@ -1,8 +1,24 @@
-export default class Movie {
+import {Component} from "react"
+import {PropTypes} from "prop-types"
+
+export default class Movie extends Component{
+
+  constructor(props) {
+    super(props)
+    console.log(props)
+  }
+
   render() {
 
     return (
-      <div></div>
+      <h4>{this.props.title}</h4>
     );
   }
+}
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
