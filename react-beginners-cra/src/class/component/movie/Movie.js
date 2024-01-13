280fe6bc941a02882fbfe9244f94cbf1aee8e9ef
webdevyoo <yjk7454@naver.com>
2024-01-13 16:09:17 +0900

    Movie컴포넌트 배열타입 genres props 추가 및 map 출력

--- react-beginners-cra/src/class/component/movie/Movie.js
+++ react-beginners-cra/src/class/component/movie/Movie.js
@@ -8,13 +8,16 @@ export default class Movie extends Component{
   } */
 
   render() {
-    const {id, year, title, summary, poster} = this.props
+    const {id, year, title, summary, poster, genres} = this.props
     return (
       <div className="movie">
         <img className="movie__poster" src={poster} alt={title} title={title}/>
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
+          <ul className='movie__genres'>
+            {genres.map((g, i) => <li key={i}>{g}</li>)}
+          </ul>
           <p className="movie__summary">{summary}</p>
         </div>
 
@@ -27,5 +30,6 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
