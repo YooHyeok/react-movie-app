981c3e133ea377ccbaae3787281babe6540a62e2
webdevyoo <yjk7454@naver.com>
2024-01-15 12:42:22 +0900

    Navigation 링크 수정

--- react-beginners-cra/src/Navigation.js
+++ react-beginners-cra/src/Navigation.js
@@ -6,7 +6,7 @@ export default class Navigation extends Component{
   render() {
     return(
     <div className="nav">
-      <Link to={`${process.env.PUBLIC_URL}/about/3`}>About</Link>
+      <Link to={`${process.env.PUBLIC_URL}/about`}>About</Link>
       <Link to={`${process.env.PUBLIC_URL}/ver1`}>Ver1</Link>
       <Link to={`${process.env.PUBLIC_URL}/ver2`}>Ver2</Link>
     </div>)
