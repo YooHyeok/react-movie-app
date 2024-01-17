dd05191bff81ac4d3916eb3cd55f14285e36c5c6
webdevyoo <yjk7454@naver.com>
2024-01-17 11:40:02 +0900

    index.js에서 App컴포넌트 impor 경로 수정

--- react-beginners-cra/src/index.js
+++ react-beginners-cra/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './AppFunc';
-import App from './AppClass';
-// import App from './App';
+// import App from './AppClass';
+import App from './App';
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
